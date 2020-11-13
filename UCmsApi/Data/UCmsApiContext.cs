9a6c6cf7b5c5c9504ac209562771d46bcbcd8b96
Almothna alkayed <eng_mothnakayed@yahoo.com>
2020-11-14 00:06:58 +0200

    My frist Teting

---
+++ UCmsApi/Data/UCmsApiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UCmsApi.Models;
+
+namespace UCmsApi.Data
+{
+    public class UCmsApiContext: DbContext
+    {
+
+        public UCmsApiContext (DbContextOptions<UCmsApiContext> options) : base(options)
+        {}
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Sidebar> Sidebars { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
+    }
+}
